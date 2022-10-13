9b048a6c7909fcd82cdf78e1157bfb479ca187d9
Willian K <billmetal9@gmail.com>
2022-10-13 15:53:41 -0300

    Estrutura Inicial

---
+++ src/main/java/br/com/homework/bill/cloudmedicalappointment/CloudMedicalAppointmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.homework.bill.cloudmedicalappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMedicalAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMedicalAppointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/homework/bill/cloudmedicalappointment/CloudMedicalAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homework.bill.cloudmedicalappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMedicalAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
